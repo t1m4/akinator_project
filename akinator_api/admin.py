3b283e7d7cca7dbc7c869f8aaa60a5e60dbb0012
Ruslan Khamidullin <ruslan.khamidullin@mint.ai>
2022-04-01 08:45:52 +0300

    Develop
    Add admin models

--- akinator_api/admin.py
+++ akinator_api/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from akinator_api import models
+
+
+class CharacterAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'image_url', 'answers']
+
+
+admin.site.register(models.Character, CharacterAdmin)
+admin.site.register(models.Question)
