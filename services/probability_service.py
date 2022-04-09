03326cd255b7636d8065972b66ff81419ed8d8b8
Ruslan Khamidullin <ruslan.khamidullin@mint.ai>
2022-04-09 14:38:04 +0300

    Delete  test probatilities

--- services/probability_service.py
+++ services/probability_service.py
@@ -84,5 +84,4 @@ class ProbabilityService(object):
             "character_total_probability": character_total_probability,
             "exclude_character_total_probability": exclude_character_total_probability,
         }
-        print("Frommyprofile", self.user_game.probabilities)
         self.user_game.save(update_fields=["probabilities"])
