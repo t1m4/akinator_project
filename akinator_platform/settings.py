14bc3243ef699099e3b9287027d60d50eeab1889
Ruslan Khamidullin <ruslan.khamidullin@mint.ai>
2022-04-09 15:14:30 +0300

    Sentry
    Add sentry integration

--- akinator_platform/settings.py
+++ akinator_platform/settings.py
@@ -14,6 +14,10 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import django_heroku
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+from sentry_sdk.integrations.celery import CeleryIntegration
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -165,3 +169,19 @@ django_heroku.settings(locals())
 
 STATIC_ROOT = BASE_DIR + '/staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Sentry
+
+sentry_sdk.init(
+    dsn="https://d4f57814a1c04460a60dedbd7175b22b@o1196583.ingest.sentry.io/6319601",
+    integrations=[DjangoIntegration(), CeleryIntegration()],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
