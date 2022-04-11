d96aef6680fa72dbaf3d25fcd075c3ca799304ee
Ruslan Khamidullin <ruslan.khamidullin@mint.ai>
2022-04-11 10:37:26 +0300

    Add new probabilities

--- services/probability_service.py
+++ services/probability_service.py
@@ -1,6 +1,8 @@
+import math
+
 import numpy as np
 
-from akinator_api import models
+from akinator_api import models, constants
 
 
 class ProbabilityService(object):
@@ -9,20 +11,21 @@ class ProbabilityService(object):
         # self.user_answers = user_answers
         self.characters_count = models.Character.objects.all().count()
         self.characters = models.Character.objects.all().values("id", "answers", "name")
+        self.is_save_probabilities = True
 
-    def calculate_probabilities(self):
+    def calculate_probabilities(self, custom_user_answer=None):
         probabilities = []
         for character in self.characters:
             probabilities.append(
                 {
                     "id": character["id"],
-                    "probability": self.calculate_character_probability(character),
+                    "probability": self.calculate_character_probability(character, custom_user_answer),
                 }
             )
 
         return probabilities
 
-    def calculate_character_probability(self, character: dict):
+    def calculate_character_probability(self, character: dict, custom_user_answer=None):
         # Prior
         init_character_probability = 1 / self.characters_count
 
@@ -37,7 +40,10 @@ class ProbabilityService(object):
             "exclude_character_total_probability", 1
         )
 
-        user_answer = self.user_game.answers[-1]
+        if self.is_save_probabilities:
+            user_answer = self.user_game.answers[-1]
+        else:
+            user_answer = custom_user_answer
         answer = float(user_answer["answer"])
         character_total_probability *= max(
             1 - abs(answer - self.character_answer(character, user_answer)), 0.01
@@ -52,19 +58,20 @@ class ProbabilityService(object):
         exclude_character_total_probability *= max(
             p_answer_not_character, np.float64(0.01)
         )
-        self.save_probabilities(
-            character, character_total_probability, exclude_character_total_probability
-        )
+        if self.is_save_probabilities:
+            self.save_probabilities(
+                character, character_total_probability, exclude_character_total_probability
+            )
 
         # Evidence
         independent_probability = (
-            character_total_probability * init_character_probability
-            + (1 - init_character_probability) * exclude_character_total_probability
+                character_total_probability * init_character_probability
+                + (1 - init_character_probability) * exclude_character_total_probability
         )
         # Bayes Theorem
         inverse_probability = (
-            character_total_probability * init_character_probability
-        ) / independent_probability
+                                      character_total_probability * init_character_probability
+                              ) / independent_probability
         return inverse_probability
 
     @staticmethod
@@ -75,13 +82,50 @@ class ProbabilityService(object):
         return 0.5
 
     def save_probabilities(
-        self,
-        character,
-        character_total_probability,
-        exclude_character_total_probability,
+            self,
+            character,
+            character_total_probability,
+            exclude_character_total_probability,
     ):
         self.user_game.probabilities[character["id"]] = {
             "character_total_probability": character_total_probability,
             "exclude_character_total_probability": exclude_character_total_probability,
         }
         self.user_game.save(update_fields=["probabilities"])
+
+    def _calculate_entropy(self, probabilities):
+        result = 0
+        for probability in probabilities:
+            result += probability['probability'] * math.log(probability['probability'], 2)
+        return result * -1
+
+    def find_next_question(self, probabilities, questions_left, answers_questions_ids):
+        current_entropy = self._calculate_entropy(probabilities)
+        characters_with_most_probability = sorted(
+            probabilities, key=lambda p: p["probability"], reverse=True
+        )[0]['id']
+        character_with_most_probability = models.Character.objects.get(id=characters_with_most_probability)
+        self.is_save_probabilities = False
+
+        best_entropy = current_entropy
+        next_question = None
+        for question in questions_left:
+            for answer in constants.ANSWER_PROBABILITY_LIST:
+                custon_user_answer = {
+                    'id': question['id'],
+                    'answer': answer,
+                }
+                new_probabilities = self.calculate_probabilities(custon_user_answer)
+                new_entropy = self._calculate_entropy(new_probabilities)
+                if new_entropy < best_entropy:
+                    best_entropy = new_entropy
+                    next_question = question
+        print("new - {}, old = {}".format(best_entropy, current_entropy))
+        if not next_question:
+            for answer in character_with_most_probability.answers:
+                character_question_id = answer['id']
+                answer_value = answer['answer']
+                if character_question_id not in answers_questions_ids and answer_value >= 0.75:
+                    next_question = questions_left.filter(id=answer['id']).values("id", "name").first()
+
+        return next_question
